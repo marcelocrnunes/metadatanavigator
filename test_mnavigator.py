2d514920a109b11c18e28b4b671ac0c2612cd8fe
Marcelo Nunes <m@mcrn.com.br>
2017-10-09 19:26:48 +0000

    More tests

--- test_mnavigator.py
+++ test_mnavigator.py
@@ -1,4 +1,6 @@
 from mnavigator import create_parser
+from metadatanavigator import get_prompt_tokens, get_toolbar_tokens
+from prompt_toolkit.token import Token
 from argparse import Namespace
 
 import unittest
@@ -6,9 +8,20 @@ import unittest
 class MnavigatorTests(unittest.TestCase):
 
     def test_create_parser(self):
-        args=create_parser()
-        self.assertIs(type(args),Namespace) 
+        result=create_parser()
+        self.assertIs(type(result),Namespace) 
 
+    def test_get_prompt_tokens(self):
+        result=get_prompt_tokens(None)
+        self.assertIs(type(result),list) 
+        for i in result: 
+            self.assertIs(i[0].parent, Token)
+
+    def test_get_toolbar_tokens(self):
+        result=get_toolbar_tokens(None)
+        self.assertIs(type(result),list) 
+        for i in result: 
+            self.assertIs(i[0].parent, Token)
 
 if __name__ == '__main__': 
     unittest.main()
